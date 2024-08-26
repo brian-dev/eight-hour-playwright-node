7b8d0ee01d378d923b5d1385d7806c79f162dedb
brian-dev <6363947+brian-dev@users.noreply.github.com>
2024-08-26 03:16:16 -0500

    set headless mode

--- playwright.config.js
+++ playwright.config.js
@@ -2,7 +2,7 @@ const config = {
     timeout: 30000,
     workers: 4,
     use: {
-        headless: false,  // Headless mode for faster execution
+        headless: true,  // Headless mode for faster execution
         viewport: { width: 1280, height: 720 },
         baseURL: 'https://www.google.com', // Set the base URL here
     },
